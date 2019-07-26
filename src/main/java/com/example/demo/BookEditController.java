9264704d1304c584195838fbf55b18cba449d3da
DimaChabai <dimatchabai@gmail.com>
2019-07-27 00:25:43 +0300

    Изменение книги

---
+++ src/main/java/com/example/demo/BookEditController.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import sun.text.normalizer.NormalizerBase;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/edit")
+public class BookEditController {
+    BooksRepository booksRepository;
+
+    public BookEditController(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+    @GetMapping
+    private ModelAndView editBookGet(@RequestParam String num,ModelAndView model){
+        Book book=booksRepository.findByNum(Long.parseLong(num)).get(0);
+        model.addObject("num",book.getNum());
+        model.addObject("name",book.getName());
+        model.setViewName("editbook");
+        return model;
+    }
+
+    @PostMapping
+    private ModelAndView editBookPost(@RequestParam String num,@RequestParam String newName ,ModelAndView model){
+        List<Book> bookList=booksRepository.findByNum(Long.parseLong(num));
+        Book book=bookList.get(0);
+        book.setName(newName);
+        booksRepository.save(book);
+
+        editBookGet(num,model);
+
+        return model;
+    }
+
+}
